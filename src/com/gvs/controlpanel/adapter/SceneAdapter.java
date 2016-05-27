9da21a7df335505436b9d2f8df26de46f6b9c690
hjy506246544 <13480253931@qq.com>
2016-05-27 19:11:26 +0800

    ·······

--- src/com/gvs/controlpanel/adapter/SceneAdapter.java
+++ src/com/gvs/controlpanel/adapter/SceneAdapter.java
@@ -20,10 +20,10 @@ import android.widget.TextView;
  */
 public class SceneAdapter extends BaseAdapter{
 
-	private Context context; //运行上下文 
-	private List listItems;//信息集合  
-	private LayoutInflater listContainer;           //视图容器 
-	
+	private Context context; //运行上下文
+	private List listItems;//信息集合
+	private LayoutInflater listContainer;           //视图容器
+
 	/**
 	 * 自定义控件集合
 	 * @author hjy
@@ -34,12 +34,12 @@ public class SceneAdapter extends BaseAdapter{
 		private ImageView iv_headView;
 	}
 
-	public SceneAdapter(Context context, List listItems) {   
-        this.context = context;            
-        listContainer = LayoutInflater.from(context);   //创建视图容器并设置上下文   
-        this.listItems = listItems; 
-    }   
-	
+	public SceneAdapter(Context context, List listItems) {
+        this.context = context;
+        listContainer = LayoutInflater.from(context);   //创建视图容器并设置上下文
+        this.listItems = listItems;
+    }
+
 	@Override
 	public int getCount() {
 		return listItems.size();
@@ -60,28 +60,28 @@ public class SceneAdapter extends BaseAdapter{
 	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		Log.e("method", "getView");   
-        final int selectID = position;   
-        //自定义视图   
-        ListItemView  listItemView = null;   
-        if (convertView == null) {   
-            listItemView = new ListItemView();    
-            //获取list_item布局文件的视图   
-            convertView = listContainer.inflate(R.layout.scene_listitem, null);   
-            //获取控件对象   
+		Log.e("method", "getView");
+        final int selectID = position;
+        //自定义视图
+        ListItemView  listItemView = null;
+        if (convertView == null) {
+            listItemView = new ListItemView();
+            //获取list_item布局文件的视图
+            convertView = listContainer.inflate(R.layout.scene_listitem, null);
+            //获取控件对象
             listItemView.iv_headView=(ImageView) convertView.findViewById(R.id.iv_headview);
             listItemView.tv_displerName=(TextView) convertView.findViewById(R.id.nametv);
-            //设置控件集到convertView   
-            convertView.setTag(listItemView);   
-        }else {   
-            listItemView = (ListItemView)convertView.getTag();   
-        }   
-        
+            //设置控件集到convertView
+            convertView.setTag(listItemView);
+        }else {
+            listItemView = (ListItemView)convertView.getTag();
+        }
+
         Map map = (Map) listItems.get(position);
         listItemView.iv_headView.setImageResource((Integer) map.get("image"));
         listItemView.tv_displerName.setText(map.get("title") + "");
-       
-        return convertView;   
+
+        return convertView;
 	}
 
 }
