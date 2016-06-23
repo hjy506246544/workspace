d3b268fa227dd53da941ff1a612b9cc5d51dad4e
hjy506246544 <13480253931@qq.com>
2016-06-23 16:19:15 +0800

    ·····

---
+++ src/com/gvs/controlpanel/bean/ItemInfo.java
@@ -0,0 +1,23 @@
+package com.gvs.controlpanel.bean;
+/**
+ *
+ * @author hjy
+ * 2016-6-23
+ */
+public class ItemInfo{
+	private String img;
+	private String text;
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
